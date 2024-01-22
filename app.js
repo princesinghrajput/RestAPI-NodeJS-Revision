a4d6bc8d3cfbff2b2ec31826ca727dbf1ca25d76
Prince5632 <prince.kumar@sensationsolutions.in>
2024-01-22 14:26:57 +0530

    Connected POSTMAN to the RestApis

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const products_routes=require("./routes/products");
+
 app.get("/", (req, res)=>{
     res.send("Hii I am Live")
 
 })
+
+app.use("/api/products", products_routes);
 
 const PORT = process.env.PORT || 5000;
 
